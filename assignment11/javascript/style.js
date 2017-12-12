13fdae70c3d6831884a2adbc5dc6851fe967f101
Anu Slorah <anuslorah@hotmail.com>
2017-12-11 16:47:01 -0800

    Delete style.js

--- assignment11/javascript/style.js
+++
@@ -1,8 +0,0 @@
-//update styling
-changeColor("#660066");
-function changeColor(hex) {
-    var elems = document.querySelectorAll("h2");
-    for (var index = 0; index < elems.length; index++) {
-        elems[index].style.color = hex;
-    }
-}
